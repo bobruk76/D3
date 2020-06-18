a931da8d07859be21cb0d5e00934f68f65dd5115
bobruk76 <vladimir.m.polyakov@gmail.com>
2020-06-18 17:49:06 +0300

    new day

--- my_site/p_library/models.py
+++ my_site/p_library/models.py
@@ -6,9 +6,17 @@ class Author(models.Model):
     birth_year = models.SmallIntegerField()
     country = models.CharField(max_length=2)
 
+    def __str__(self):
+        return self.full_name
+
 class Book(models.Model):
     ISBN = models.CharField(max_length=13)
     title = models.TextField()
     description = models.TextField()
     year_release = models.SmallIntegerField()
+    copy_count = models.SmallIntegerField(default=0)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=15)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{}--{}".format(self.title, self.author)
